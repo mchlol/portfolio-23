98fd30cf0fb04170fa048caedff28c36c85b97de
Michelle Brown <m.andreabr@gmail.com>
2023-10-27 16:53:26 +1100

    remove marquee

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import './App.css'
 import Nav from './components/nav'
 import Hero from './components/Hero'
-// import SkillsMarquee from './components/SkillsMarquee'
 import Projects from './components/Projects'
 import Footer from './components/Footer'
 
