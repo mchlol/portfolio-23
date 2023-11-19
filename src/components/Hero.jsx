698f105a672587e8739c337ff2e7b4272b0a1f54
Michelle Brown <m.andreabr@gmail.com>
2023-11-20 09:55:20 +1100

    add toolbox and icons

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,9 @@
 import React from "react";
-
 import SparksSmall from '../assets/sparks-small.png'
-import Sparks from '../assets/sparks.png'
+import { FaFigma } from "react-icons/fa6";
+import { DiHtml5, DiCss3, DiJsBadge, DiReact, DiVisualstudio, DiGit, DiGithubBadge } from "react-icons/di";
+import { SiNextdotjs, SiDaisyui, SiTailwindcss, SiWordpress, SiNotion } from "react-icons/si";
+import { FiTerminal } from "react-icons/fi";
 
 
 export default function Hero() {
@@ -14,11 +16,36 @@ export default function Hero() {
             <div className="hero--text">
                 <h1>Hello! 👋
                 <br />
-                I'm Michelle
+                I'm <span className="text-outline">Michelle</span>
                 </h1>
                 <img className="sparks" src={SparksSmall} alt="sparkles"/>
 
                 <h2>I'm a front-end developer in Melbourne, Australia.</h2>
+
+            </div>
+
+            <div className="tools-container">
+                <h3>Tools & Technologies</h3>
+
+                <div className="tools-container-icons">
+                    <span><DiHtml5 /> HTML5</span>
+                    <span><DiCss3 /> CSS3</span>
+                    <span><DiJsBadge /> JavaScript</span>
+                    <span><DiReact /> React</span>
+                    <span><SiTailwindcss /> TailwindCSS</span>
+                    <span><SiDaisyui /> DaisyUI</span>
+
+                    <span><DiGit /> Git</span>
+                    <span><DiGithubBadge /> GitHub</span>
+                    <span><DiVisualstudio /> VS Code</span>
+                    <span><FiTerminal /> iTerm</span>
+                    <span><FaFigma /> Figma</span>
+                    <span><SiNotion /> Notion</span>
+
+                    <span><SiWordpress /> WordPress</span>
+                    <span><SiNextdotjs /> Next</span>
+                </div>
+                
             </div>
             
         </div>
