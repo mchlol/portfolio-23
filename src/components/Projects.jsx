3282e8fa514ca09594da80f33444a32772c74698
Michelle Brown <m.andreabr@gmail.com>
2023-11-19 17:41:43 +1100

    add new projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,15 +6,13 @@ import tictacGif from '../assets/project-tictactoe.gif';
 import superdrawGif from '../assets/project-superdraw.gif';
 import tenziesGif from '../assets/project-tenzies.gif';
 import passwordGif from '../assets/project-password-generator.gif';
-
-
-
-
+import quizzicalGif from '../assets/project-quizzical.gif'
+import colorsGif from '../assets/project-colors.gif'
 
 export default function Projects() {
     return (
         <section className="projects-section">
-            <h2>Projects</h2>
+            <h2 id="#projects">Projects</h2>
 
             <div className="project-wrapper">
 
@@ -22,40 +20,67 @@ export default function Projects() {
 
                     <h3>Game List</h3>
                     <p>Capstone project for the <a href="https://generalassemb.ly/education/javascript-development/sydney" target="_blank">JavaScript Development</a> course at General Assembly.</p>
-                    <p>A web app for users to save games from across different platforms to a series of lists, such as wishlist or favourites.<br />
-                    Built with React and React Router, along with Tailwind CSS and DaisyUI for React. Data is from the RAWG.io API.</p>
+                    <p>A web app for users to save games from across different platforms to their wishlist or favourites.<br />
+                    Originally built with React & React Router, now with Next, along with Tailwind CSS and DaisyUI. Data is from the RAWG.io API.</p>
 
                 </div>
 
                 <div className="project-img-container">
                     <img className="project-img" src={gameListScreenshot} alt="screenshot of a list of video games" />
-                    <a href="https://github.com/mchlol/jsd-game-list" target="_blank"><button>View on GitHub</button></a>
+                    <a href="https://mchlol-gamelist.netlify.app/"><button>Live</button></a>
+                    <a href="https://github.com/mchlol/next-game-list" target="_blank"><button>Code</button></a>
                 </div>
 
                 <div className="projects-group">
+                <div className="resize-project project-img-container">
+                        <h3>Colors</h3>
+                        <img className="project-img" src={colorsGif} alt="" />
+                        <a href="https://mchlol-next-colors.netlify.app/" target="_blank"><button>Live</button>
+                        </a>
+                        <a href="https://github.com/mchlol/colors" target="_blank"><button>Code</button></a>
+                    </div>
                     <div className="resize-project project-img-container">
+                        <h3>Quizzical</h3>
+                        <img className="project-img" src={quizzicalGif} alt="" />
+                        <a href="https://mchlol-quiz.netlify.app/" target="_blank"><button>Live</button>
+                        </a>
+                        <a href="https://github.com/mchlol/quizzical" target="_blank"><button>Code</button></a>
+                    </div>
+                    <div className="resize-project project-img-container">
+                    <h3>Tenzies</h3>
                         <img className="project-img" src={tenziesGif} alt="" />
-                        <a href="https://github.com/mchlol/tenzies" target="_blank"><button>View on GitHub</button></a>
+                        <a href="https://mchlol.github.io/tenzies/" target="_blank"><button>Live</button></a>
+                        <a href="https://github.com/mchlol/tenzies" target="_blank"><button>Code</button></a>
                     </div>
                     <div className="resize-project project-img-container">
+                        <h3>Password Generator</h3>
                         <img className="project-img" src={passwordGif} alt="" />
-                        <a href="https://github.com/mchlol/password-generator" target="_blank"><button>View on GitHub</button></a>
+                        <a href="https://mchlol.github.io/password-generator/" target="_blank"><button>Live</button></a>
+                        <a href="https://github.com/mchlol/password-generator" target="_blank"><button>Code</button></a>
                     </div>
                     <div className="resize-project project-img-container">
+                        <h3>Library</h3>
                         <img className="project-img" src={libraryGif} alt="" />
-                        <a href="https://github.com/mchlol/library" target="_blank"><button>View on GitHub</button></a>
+                        <a href="https://mchlol.github.io/library/" target="_blank"><button>Live</button></a>
+                        <a href="https://github.com/mchlol/library" target="_blank"><button>Code</button></a>
                     </div>
                     <div className="resize-project project-img-container">
+                        <h3>Pixel Sketchpad</h3>
                         <img className="project-img" src={pixelGif} alt="" />
-                        <a href="https://github.com/mchlol/etchasketch" target="_blank"><button>View on GitHub</button></a>
+                        <a href="https://mchlol.github.io/etchasketch/" target="_blank"><button>Live</button></a>
+                        <a href="https://github.com/mchlol/etchasketch" target="_blank"><button>Code</button></a>
                     </div>
                     <div className="resize-project project-img-container">
+                        <h3>TicTacToe</h3>
                         <img className="project-img" src={tictacGif} alt="" />
-                        <a href="https://github.com/mchlol/tictactoe" target="_blank"><button>View on GitHub</button></a>
+                        <a href="https://mchlol.github.io/tictactoe/" target="_blank"><button>Live</button></a>
+                        <a href="https://github.com/mchlol/tictactoe" target="_blank"><button>Code</button></a>
                     </div>
                     <div className="resize-project project-img-container">
+                        <h3>SuperDraw</h3>
                         <img className="project-img" src={superdrawGif} alt="" />
-                        <a href="https://github.com/mchlol/superdraw" target="_blank"><button>View on GitHub</button></a>
+                        <a href="https://mchlol.github.io/superdraw/" target="_blank"><button>Live</button></a>
+                        <a href="https://github.com/mchlol/superdraw" target="_blank"><button>Code</button></a>
                     </div>
                     
                 </div>
