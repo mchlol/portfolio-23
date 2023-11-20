ae00abae746342ff48c09ae5401322f656312983
Michelle Brown <m.andreabr@gmail.com>
2023-11-20 15:03:49 +1100

    temporarily remove some projects, update game list project to next version

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,14 +1,16 @@
 import React from "react";
-import gameListScreenshot from '../assets/game-list.png'
-import libraryGif from '../assets/project-library.gif';
-import pixelGif from '../assets/project-pixel-sketchpad.gif';
-import tictacGif from '../assets/project-tictactoe.gif';
-import superdrawGif from '../assets/project-superdraw.gif';
+import gamelistGif from '../assets/project-gamelist.gif';
+// import libraryGif from '../assets/project-library.gif';
+// import pixelGif from '../assets/project-pixel-sketchpad.gif';
+// import tictacGif from '../assets/project-tictactoe.gif';
+// import superdrawGif from '../assets/project-superdraw.gif';
 import tenziesGif from '../assets/project-tenzies.gif';
 import passwordGif from '../assets/project-password-generator.gif';
 import quizzicalGif from '../assets/project-quizzical.gif'
 import colorsGif from '../assets/project-colors.gif'
 import { MdCode, MdVisibility } from "react-icons/md"
+import Sparks from '../assets/Sparks.png'
+
 
 export default function Projects() {
     return (
@@ -23,20 +25,24 @@ export default function Projects() {
 
                     <div className="project-img-container">
 
-                        <img className="project-img" src={gameListScreenshot} alt="screenshot of a list of video games" />
+                        <img className="project-img" src={gamelistGif} alt="screenshot of a list of video games" />
 
-                        <a href="https://jsd-game-list.netlify.app" target="_blank">
+                        <a href="https://mchlol-gamelist.netlify.app/" target="_blank">
                             <button><MdVisibility /> Live</button>
                         </a>
-                        <a href="https://github.com/mchlol/jsd-game-list" target="_blank">
+                        <a href="https://github.com/mchlol/next-game-list" target="_blank">
                             <button><MdCode />Code</button>
                         </a>
 
                     </div>
 
-                    <p>Capstone project for the <a href="https://generalassemb.ly/education/javascript-development/sydney" target="_blank">JavaScript Development</a> course at General Assembly. We were tasked with building a site to display data from any API of our choosing. I created a web app for users to save games from across different platforms to their wishlist or favourites.<br />
-                    Built with React & React Router, along with Tailwind CSS and DaisyUI. Currently working on switching to Next.</p>
+                    <p>Capstone project for the <a href="https://generalassemb.ly/education/javascript-development/sydney" target="_blank">JavaScript Development</a> course at General Assembly. I created a web app using data from the RAWG.io API, for users to search for games from across different platforms & save to their wishlist or favourites.<br />
+                    Originally built with React & React Router then rebuilt in Next, along with Tailwind CSS and DaisyUI, with continuous deployment from a GitHub repo to Netlify.</p>
 
+                </div>
+
+                <div className="project-sparks-container">
+                    <img className="sparks" src={Sparks} />
                 </div>
 
                 <div className="projects-group">
@@ -67,7 +73,7 @@ export default function Projects() {
                         <a href="https://mchlol.github.io/password-generator/" target="_blank"><button><MdVisibility /> Live</button></a>
                         <a href="https://github.com/mchlol/password-generator" target="_blank"><button><MdCode />Code</button></a>
                     </div>
-                    <div className="resize-project project-img-container">
+                    {/* <div className="resize-project project-img-container">
                         <h3>Library</h3>
                         <img className="project-img" src={libraryGif} alt="" />
                         <a href="https://mchlol.github.io/library/" target="_blank"><button><MdVisibility /> Live</button></a>
@@ -90,7 +96,7 @@ export default function Projects() {
                         <img className="project-img" src={superdrawGif} alt="" />
                         <a href="https://mchlol.github.io/superdraw/" target="_blank"><button><MdVisibility /> Live</button></a>
                         <a href="https://github.com/mchlol/superdraw" target="_blank"><button><MdCode />Code</button></a>
-                    </div>
+                    </div> */}
                     
                 </div>
                     
