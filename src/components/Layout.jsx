286d642e6da4d30805877100f25dc2986660aef5
Michelle Brown <m.andreabr@gmail.com>
2024-01-26 13:56:17 +1100

    set up router, move page components to pages directory, define the layout, update imports

---
+++ src/components/Layout.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Nav from "./Nav";
+import Footer from "./Footer";
+import { Outlet } from "react-router-dom";
+
+export default function Layout() {
+    return (
+        <>
+            <Nav />
+            <Outlet />
+            <Footer />
+        </>
+    )
+}
