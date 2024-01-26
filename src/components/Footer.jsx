6476bee439916eb2c85719ac9b95edbf89d2f1f0
Michelle Brown <m.andreabr@gmail.com>
2024-01-26 15:30:20 +1100

    add rel to links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,10 +6,10 @@ export default function Footer() {
     return (
         <footer>
             <ul>
-                <li className="social-icon"><a href="https://github.com/mchlol" target="_blank">
+                <li className="social-icon"><a href="https://github.com/mchlol" target="_blank" rel="noopener noreferrer">
                     <FaGithub />
                 </a></li>
-                <li className="social-icon"><a href="https://www.linkedin.com/in/michellebrown01/" target="_blank">
+                <li className="social-icon"><a href="https://www.linkedin.com/in/michellebrown01/" target="_blank" rel="noopener noreferrer">
                     <FaLinkedin />
                 </a></li>
             </ul>
