2471deac15bddf524c819bf3cdd19668a44267c3
Michelle Brown <m.andreabr@gmail.com>
2024-02-11 15:40:03 +1100

    create hero feature component and import sections

---
+++ src/components/Feature.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import ShadowBox from './ShadowBox';
+import Sparks from "./Sparks"
+import Computer from '../assets/noun-computer.svg';
+import TypingText from './TypingText';
+
+export default function Feature() {
+    return (
+        <div className="feature">
+            <ShadowBox classNames="feature--top">
+                <Sparks />
+            </ShadowBox>
+
+            <ShadowBox classNames="feature--center">
+                <h1 style={{textAlign: "right", margin: "0"}}>Hello! <span className="gradient-text">Salut!</span></h1>
+            </ShadowBox>
+
+            {/* <ShadowBox classNames="feature--bLeft">
+                <img src={Computer} alt="line drawing of a 90s era computer"/>
+            </ShadowBox> */}
+
+            <ShadowBox classNames="feature--bRight">
+                <div className="terminal">
+                    <TypingText />
+                </div>
+            </ShadowBox>
+        </div>
+    )
+}
+
