33f8ef59fcfe37d0ed5736195d3e62020fe320a3
Michelle Brown <m.andreabr@gmail.com>
2024-02-12 12:24:24 +1100

    set hero top to flex row wrap

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -19,20 +19,16 @@ export default function Hero() {
 
         <div className="hero--content">
 
-            <Feature />
-
-            <div className="hero--text">
-
-                <h2>I'm <span className="text-outline">Michelle</span></h2>
-
-                <p style={ {fontSize: "1.5rem"}}>I'm a front-end developer in Melbourne, Australia.</p>
-
-                <p>I love design, coding, and learning to design and code cool stuff.</p>
-
-                <p>
-                <Link to="/about">Learn more about me</Link> or <Link to="/projects">see some things I've made</Link>.
-                </p>
-
+            <div className="hero--top">
+                <Feature />
+                <div className="hero--text">
+                    <h2>I'm <span className="text-outline">Michelle</span></h2>
+                    <p style={ {fontSize: "1.5rem"}}>I'm a front-end developer in Melbourne, Australia.</p>
+                    <p>I love design, coding, and learning to design and code cool stuff.</p>
+                    <p>
+                    <Link to="/about">Learn more about me</Link> or <Link to="/projects">see some things I've made</Link>.
+                    </p>
+                </div>
             </div>
 
             <div className="tools-container gradient-border">
