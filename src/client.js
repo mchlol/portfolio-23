d7f744956abf22e4fd71c5e24c6aa77db7db389e
Michelle Brown <m.andreabr@gmail.com>
2024-05-11 13:51:54 +1000

    set up variables

--- src/client.js
+++ src/client.js
@@ -1,11 +1,11 @@
 import { createClient } from "@sanity/client";
 
 const client = createClient({
-  projectId: "52duxrdl",
+  projectId: import.meta.env.VITE_REACT_APP_PROJECTID,
   dataset: "production",
   apiVersion: "2024-04-28",
   // unless you have caching for your front end, `useCdn` should be `true` for most production environments
-  useCdn: false, 
+  useCdn: true, 
 })
 
 export default client;
