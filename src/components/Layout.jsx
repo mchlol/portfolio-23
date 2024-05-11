2a300ce7c9790247aa21f63888f93694458d72b0
Michelle Brown <m.andreabr@gmail.com>
2024-05-11 13:52:59 +1000

    add min height to layout wrapper

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,10 +5,12 @@ import { Outlet } from "react-router-dom";
 
 export default function Layout() {
     return (
-        <>
+        <div style={ {
+            minHeight: "100vh"
+        }}>
             <Nav />
             <Outlet />
             <Footer />
-        </>
+        </div>
     )
 }
