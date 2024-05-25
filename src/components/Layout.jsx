095b9c14e6a031cfa476ecac8d8960cfda04f103
Michelle Brown <m.andreabr@gmail.com>
2024-05-25 15:41:04 +1000

    make nav sticky and footer fixed

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -6,8 +6,10 @@ import { Outlet } from "react-router-dom";
 export default function Layout() {
     return (
         <div className="layout">
+        <div className="topContent">
             <Nav />
             <Outlet />
+        </div>
             <Footer />
         </div>
     )
