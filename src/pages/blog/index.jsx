f22d687bf1a11a24f9cf088e4f8eba8fbe3c7aaa
Michelle Brown <m.andreabr@gmail.com>
2024-05-25 17:36:08 +1000

    swap loading conditional

--- src/pages/blog/index.jsx
+++ src/pages/blog/index.jsx
@@ -46,7 +46,7 @@ export default function BlogHome() {
 
                 <div className="blog-wrapper text-center">
                 {
-                    !loading
+                    loading
                     ? <Loading />
                     :
                     posts.length > 0
