4e265d826e32de23eaa67175a837930eb3344a98
Michelle Brown <m.andreabr@gmail.com>
2024-06-08 10:18:36 +1000

    fix typo

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -32,7 +32,7 @@ export default function About() {
                 <div className="about-section-text">
                     <h3>What is a mchlol?</h3>
                     <p>As a teen, I ventured into web design armed with Notepad, Microsoft FrontPage, and of course GeoCities. Despite basically growing up on the computer I didn't think I was smart enough to even consider it as a career!</p>
-                    <p>In my day job working in print many years later, I took over the company's WordPress site, diving into theme customization and SEO. This reignited a passion for web design,and I thought, "why not give coding a crack?" This trail lead through The Odin Project, JavaScript development at General Assembly and Scrimba's Front End Developer Career Path. Me and everyone else.</p> 
+                    <p>In my day job working in print many years later, I took over the company's WordPress site, diving into theme customisation and SEO. This reignited a passion for web design,and I thought, "why not give coding a crack?" This trail lead through The Odin Project, JavaScript development at General Assembly and Scrimba's Front End Developer Career Path. Me and everyone else.</p> 
                     <p>Now knee-deep in code, I'm exploring APIs, NextJS, Node, Python, and beyond.<br />
                     I love to learn and I love the web. 🤝 </p>
                     <p>Outside the digital realm, I dabble in <em>apprendre à parler français</em>, font appreciation, gaming, baking and the occasional embroidery.</p>
